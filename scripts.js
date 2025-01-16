2d9f693db99723f2a7faf4381ac398327523888e
Lucas Gabriel <gomesborgesbiel@gmail.com>
2025-01-16 15:17:38 -0300

    tst

--- scripts.js
+++ scripts.js
@@ -3,20 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const artistsData = [
 
         { name: 'Settimo', image: './img/FOTO-DE-PERFIL.png' },
-        { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpeg' },
-        { name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpeg' },
-        { name: 'Gusttavo Lima', image: './img/artista-gustavo-limma.jpeg'},
-        { name: 'Luan Santana', image: './img/artista-luan-santana.jpeg' }, 
-        { name: 'Matheus & Kauan', image: './img/artista-mateus-kauan.jpeg' } 
+        { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpg' },
+        { name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpg' },
+        { name: 'Gusttavo Lima', image: './img/artista-gustavo-limma.jpg'},
+        { name: 'Luan Santana', image: './img/artista-luan-santana.jpg' }, 
+        { name: 'Matheus & Kauan', image: './img/artista-mateus-kauan.jpg' } 
         ];
 
         const albumsData = [
         { name: 'Arquivos Locais', artist: 'Settimo', image: './img/CAPA.png' },
-        { name: 'O Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano',image: './img/album-ceu-explica.jpeg' },
-        { name: 'Nada como um dia após o outro', artist: 'Racionais',image: './img/album-vida-loka.jpeg' },
-        { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image:'./img/album-hit-me.jpeg' },
-        { name: 'CAJU', artist: 'Liniker', image: './img/album-caju.jpeg' },
-        { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image:'./img/album-escandalo.jpeg' }
+        { name: 'O Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano',image: './img/album-ceu-explica.jpg' },
+        { name: 'Nada como um dia após o outro', artist: 'Racionais',image: './img/album-vida-loka.jpg' },
+        { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image:'./img/album-hit-me.jpg' },
+        { name: 'CAJU', artist: 'Liniker', image: './img/album-caju.jpg' },
+        { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image:'./img/album-escandalo.jpg' }
         ];
 
 const artistGrid = document.querySelector('.artists-grid')
